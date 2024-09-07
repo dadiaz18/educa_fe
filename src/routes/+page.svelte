83b0c00a3bfe55eda79cd7579d142c146e3685b8
Diego Andres Diaz <ddiaz@Diegos-MacBook-Pro.local>
2024-09-07 19:32:56 +0200

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<h1 class="tw-text-3xl tw-font-bold tw-underline">Hello world!</h1>
+
+<style lang="postcss">
+	:global(html) {
+		background-color: theme(colors.gray.100);
+	}
+</style>
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  prefix: 'tw-',
+  content: ['./src/**/*.{html,js,svelte,ts}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
